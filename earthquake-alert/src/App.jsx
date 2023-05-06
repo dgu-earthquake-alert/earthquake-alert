04b9834908cc733aab13684d765a8cf9a00e1488
star2t <star2thb@gmail.com>
2023-05-06 19:50:12 +0900

    Fix: 컴포넌트 이름 수정
    Googlemap 컴포넌트 이름을 GoogleMap으로 수정했습니다.

--- earthquake-alert/src/App.jsx
+++ earthquake-alert/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import GoogleMap from "./components/Googlemap";
+import GoogleMap from "./components/GoogleMap";
 import "./styles/App.css";
 
 
