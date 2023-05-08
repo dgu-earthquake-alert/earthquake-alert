7162c9bfb62bda784bacf4245f7478958467f825
Kong-E <kng001016@dgu.ac.kr>
2023-05-08 15:08:47 +0900

    Feat: 현재위치 Refresh 버튼 추가

--- earthquake-alert/src/pages/Home.jsx
+++ earthquake-alert/src/pages/Home.jsx
@@ -7,9 +7,10 @@ import "../styles/Sidebar.css";
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("위치정보없음");
   const [lat, setLat] = useState(37.569227); // 위도
   const [lng, setLng] = useState(126.9777256); // 경도
+  const [isRotated, setIsRotated] = useState(false); // 새로고침버튼 회전
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   let shelterNumber = 1; // css position 계산용
@@ -24,10 +25,22 @@ function Home() {
   };
 
   const getMyLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
+    function onGeoOK(position) {
       setLat(position.coords.latitude);
       setLng(position.coords.longitude);
-    });
+    }
+    function onGeoError() {
+      setLocation("위치정보없음");
+    }
+    navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
+  };
+
+  const refresh = () => {
+    setIsRotated(true);
+    getMyLocation();
+    setTimeout(() => {
+      setIsRotated(false);
+    }, 500);
   };
 
   useEffect(() => {
@@ -68,13 +81,15 @@ function Home() {
           ⭐
         </button>
         <div className={`sidebar ${isOpen ? "open" : ""}`}>
-          <div className="bookmark_add">+</div>
-          <div className="bookmark_remove">-</div>
+          <div
+            className={`bookmark_refresh ${isRotated ? "rotate" : ""}`}
+            onClick={refresh}
+          ></div>
+          <div className="bookmark_add"></div>
+          <div className="bookmark_remove"></div>
           <div className="my_location">
             <span className="my_location_title">현재 위치</span>
-            {location !== "" || (lat !== 37.569227 && lng !== 126.9777256)
-              ? location
-              : "위치 정보 없음"}
+            {location}
           </div>
           <div className="my_location_item" style={{ top: `${top}px` }}>
             대피소 {shelterNumber++}
