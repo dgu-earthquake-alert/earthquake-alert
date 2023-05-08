81956989183a10cecee21b452a6000e76a87a860
Kong-E <kng001016@dgu.ac.kr>
2023-05-08 16:54:23 +0900

    Feat: 라우터 삭제

--- earthquake-alert/src/index.jsx
+++ earthquake-alert/src/index.jsx
@@ -2,19 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </React.StrictMode>
 );
 
