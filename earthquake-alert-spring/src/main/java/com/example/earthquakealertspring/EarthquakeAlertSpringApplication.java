2879e79a6a367d8a41928321751f71f2409fdbc8
Subin <ben7232@gmail.com>
2023-05-10 16:42:58 +0900

    Build: 빈 스프링 프로젝트 생성
    JDK 11(temurine)
    Springboot : 2.7.12
    Gradle JVM : temurine-11 java version "11.0.19"
    initial dependency : Spring web

---
+++ earthquake-alert-spring/src/main/java/com/example/earthquakealertspring/EarthquakeAlertSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthquakealertspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeAlertSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeAlertSpringApplication.class, args);
+	}
+
+}
