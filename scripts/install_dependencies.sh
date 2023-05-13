9c4b38b4bc601b4e5db85abb3da3fdf8bbffbc7b
Subin <ben7232@gmail.com>
2023-05-13 21:29:09 +0900

    Initial commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,4 +5,11 @@ then
     sudo yum install -y docker
     sudo usermod -a -G docker ec2-user
 fi
+if ! command -v aws &> /dev/null
+then
+    sudo yum install -y awscli
+fi
 sudo service docker start
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 646695010239.dkr.ecr.ap-southeast-2.amazonaws.com
+docker pull 646695010239.dkr.ecr.ap-southeast-2.amazonaws.com/earthquake-alert-backend-ecr:backend-latest
+docker pull 646695010239.dkr.ecr.ap-southeast-2.amazonaws.com/earthquake-alert-frontend-ecr:frontend-latest
