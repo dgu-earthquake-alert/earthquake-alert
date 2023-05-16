5383c740a5abea29a4cdf7e1aaba000d0a508bf0
star2t <star2thb@gmail.com>
2023-05-16 10:47:04 +0900

    Chore: bundle.js 오류 수정

--- earthquake-alert/src/components/home/GoogleMap.jsx
+++ earthquake-alert/src/components/home/GoogleMap.jsx
@@ -12,30 +12,15 @@ const GoogleMap = ({ lat, lng, handleMapClick }) => {
     const script = document.createElement("script");
     script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
     script.async = true;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (map && lat && lng) {
-      map.setCenter({ lat, lng });
-    }
-  }, [map, lat, lng]);
-
+    
   window.initMap = async () => {
-    const newMap = new window.google.maps.Map(ref.current, {
-      center: { lat, lng },
-      zoom: 16,
-    });
-    setMap(newMap);
-
-    newMap.addListener("click", handleMapClick); // Add click event listener to the map
-
     try {
       const shelterData = await fetchMapPlaceData();
+
+      const newMap = new window.google.maps.Map(ref.current, {
+        center: { lat, lng },
+        zoom: 16,
+      });
 
       shelterData.forEach((shelter) => {
         const marker = new window.google.maps.Marker({
@@ -50,6 +35,56 @@ const GoogleMap = ({ lat, lng, handleMapClick }) => {
           },
         });
       });
+
+      setMap(newMap);
+      newMap.addListener("click", handleMapClick);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  document.head.appendChild(script);
+
+  return () => {
+    document.head.removeChild(script);
+  };
+}, []);
+
+  useEffect(() => {
+    if (map && lat && lng) {
+      map.setCenter({ lat, lng });
+    }
+  }, [map, lat, lng]);
+
+  window.initMap = async () => {
+    try {
+      const shelterData = await fetchMapPlaceData();
+  
+      const newMap = new window.google.maps.Map(ref.current, {
+        center: { lat, lng },
+        zoom: 16,
+      });
+  
+      shelterData.forEach((shelter) => {
+        const marker = new window.google.maps.Marker({
+          position: { lat: shelter.lat, lng: shelter.lng },
+          map: newMap,
+          title: shelter.name,
+          icon: {
+            url: process.env.PUBLIC_URL + "/images/marker.png",
+            scaledSize: new window.google.maps.Size(40, 40),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(25, 50),
+          },
+        });
+      });
+  
+      if (newMap instanceof window.google.maps.Map) {
+        setMap(newMap);
+        newMap.addListener("click", handleMapClick);
+      } else {
+        console.error("Invalid map instance");
+      }
     } catch (error) {
       console.error(error.message);
     }
