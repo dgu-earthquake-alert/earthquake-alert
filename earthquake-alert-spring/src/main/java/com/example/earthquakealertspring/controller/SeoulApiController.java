d2f6054e8b5d5de53fa7f9fab1ebf606d2d473b1
Subin <ben7232@gmail.com>
2023-05-18 19:55:37 +0900

    Feat: 서울시 데이터 HTTPS로 받아오기 기능구현
    BASE_URL = "https://server.earthquake-alert.site/api/"로 변경되었습니다.

---
+++ earthquake-alert-spring/src/main/java/com/example/earthquakealertspring/controller/SeoulApiController.java
@@ -0,0 +1,23 @@
+package com.example.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+public class SeoulApiController {
+    private final RestTemplate restTemplate;
+
+    public ApiController() {
+        this.restTemplate = new RestTemplate();
+    }
+    @RequestMapping("/api/{apiKey}/json/{pathVar1}/{pathVar2}/{pathVar3}")
+    public ResponseEntity<Map> fetchApiData(@PathVariable String apiKey, @PathVariable String pathVar1, @PathVariable String pathVar2, @PathVariable String pathVar3) {
+        String url = "http://openapi.seoul.go.kr:8088/" + apiKey + "/json/" + pathVar1 + "/" + pathVar2 + "/" + pathVar3;
+        return restTemplate.getForEntity(url, Map.class);
+    }
+}
