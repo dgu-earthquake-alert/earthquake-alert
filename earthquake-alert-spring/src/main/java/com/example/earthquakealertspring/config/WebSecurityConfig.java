e384cebf2eecb3603ff2d239bf0a8beaae500375
Hankyul Min <denirokr22@gmail.com>
2023-05-20 21:46:07 +0900

    Feat: 소셜로그인 WebSecurityConfig

---
+++ earthquake-alert-spring/src/main/java/com/example/earthquakealertspring/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.earthquakealertspring.config;
+
+import com.example.earthquakealertspring.service.oauth2.CustomOAuth2UserService;
+import com.example.earthquakealertspring.service.security.JwtAuthenticationFilter;
+import com.example.earthquakealertspring.service.security.OAuth2SuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
+    @Autowired
+    private OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors()
+                .and()
+                .csrf()
+                .disable()
+                .httpBasic()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/auth/**", "/oauth2/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*")
+                .and()
+                .authorizationEndpoint()
+                .baseUri("/auth/authorize")
+                .and()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService)
+                .and()
+                .successHandler(oAuth2SuccessHandler)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+
+        httpSecurity.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+    }
+}
