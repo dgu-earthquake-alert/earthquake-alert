b5100acc653d9e0373f6cb3c43b0e7fde4b5493e
Subin <ben7232@gmail.com>
2023-05-23 01:11:41 +0900

    Chore: apiClient로 api호출 관리

--- earthquake-alert/src/utils/api.js
+++ earthquake-alert/src/utils/api.js
@@ -1,18 +1,38 @@
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "https://server.earthquake-alert.site/api/";
-
+//배포
+//const BASE_URL = "https://server.earthquake-alert.site/api/";
+const BASE_URL = "http://localhost:8081/api/";
 // 옥외대피소 API: outdoorResponse, 실내구호소 API: indoorResponse
+
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // 쿠키를 요청에 포함합니다.
+});
+
+export const sendTokenToServer = (token) => {
+  apiClient
+    .post("/register-token", {
+      token: token,
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 export const fetchMapPlaceData = async () => {
   try {
-    const outdoorResponse = await axios.get(
-      `${BASE_URL}${API_KEY}/json/TlEtqkP/1/1000/`
+    const outdoorResponse = await apiClient.get(
+      `${API_KEY}/json/TlEtqkP/1/1000/`
     );
     const outdoorData = outdoorResponse.data;
 
-    const indoorResponse = await axios.get(
-      `${BASE_URL}${API_KEY}/json/TlInetqkP/1/1000/`
+    const indoorResponse = await apiClient.get(
+      `${API_KEY}/json/TlInetqkP/1/1000/`
     );
     const indoorData = indoorResponse.data;
 
@@ -62,9 +82,9 @@ export const fetchMapPlaceData = async () => {
 
 export const fetchShelterTableData = async (gu, dong) => {
   try {
-    const response = await axios.get(
+    const response = await apiClient.get(
       //"http://openapi.seoul.go.kr:8088/66524245416c736239334a75697446/json/TlEtqkP/1/1000/"
-      `${BASE_URL}${API_KEY}/json/TlEtqkP/1/1000/`
+      `${API_KEY}/json/TlEtqkP/1/1000/`
     );
     const data = response.data;
 
@@ -90,8 +110,8 @@ export const fetchShelterTableData = async (gu, dong) => {
 };
 export const fetchRecordTableData = async (si, startDate, endDate) => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}${API_KEY}/json/TbEqkKenvinfo/1/1000/`
+    const response = await apiClient.get(
+      `${API_KEY}/json/TbEqkKenvinfo/1/1000/`
     );
     const data = response.data;
     if (
@@ -131,17 +151,4 @@ export const fetchRecordTableData = async (si, startDate, endDate) => {
     alert(`API 요청 중 에러가 발생했습니다: ${error.message}`);
     return [];
   }
-};
-
-export const sendTokenToServer = (token) => {
-  axios
-    .post("https://server.earthquake-alert.site/register-token", {
-      token: token,
-    })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
 };
