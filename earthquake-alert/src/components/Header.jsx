edc96471e83ace3c15bbce81a56145f78124c1c2
Kong-E <kng001016@dgu.ac.kr>
2023-05-23 17:43:21 +0900

    Style: 모바일 미디어쿼리 - 헤더

--- earthquake-alert/src/components/Header.jsx
+++ earthquake-alert/src/components/Header.jsx
@@ -4,9 +4,13 @@ import styles from "../styles/header.module.css";
 /* import Login from "./login/Login";
 import Profile from "./login/Profile";
  */
+import { Mobile, PC } from "./MediaQuery";
+import menu from "../assets/icon/menu.svg";
+
 const Header = ({ isSidebarOpen, toggleHeader }) => {
   const location = useLocation();
   const [selectedItem, setSelectedItem] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
   const [userInfo, setUserInfo] = useState({});
 
@@ -15,46 +19,93 @@ const Header = ({ isSidebarOpen, toggleHeader }) => {
   }, [location]);
 
   return (
-    <nav className={`${styles.nav} ${isSidebarOpen ? styles.open : ""}`}>
-      <Link to="/" className={styles.nav_title}>
-        지진알리미
-      </Link>
-      <Link
-        to="/shelter"
-        className={`${styles.nav_item} ${
-          selectedItem === "/shelter" ? styles.nav_item_select : ""
-        }`}
-      >
-        지진대피소 조회
-      </Link>
-      <Link
-        to="/record"
-        className={`${styles.nav_item} ${
-          selectedItem === "/record" ? styles.nav_item_select : ""
-        }`}
-      >
-        지진발생 이력
-      </Link>
-      <Link
-        to="/rule"
-        className={`${styles.nav_item} ${
-          selectedItem === "/rule" ||
-          selectedItem === "/rule/subpage1" ||
-          selectedItem === "/rule/subpage2"
-            ? styles.nav_item_select
-            : ""
-        }`}
-      >
-        행동요령
-      </Link>
-      {/*       <div className={`${styles.nav_login}`}>
+    <>
+      <PC>
+        <nav className={`${styles.nav} ${isSidebarOpen ? styles.open : ""}`}>
+          <Link to="/" className={styles.nav_title}>
+            지진알리미
+          </Link>
+          <Link
+            to="/shelter"
+            className={`${styles.nav_item} ${
+              selectedItem === "/shelter" ? styles.nav_item_select : ""
+            }`}
+          >
+            지진대피소 조회
+          </Link>
+          <Link
+            to="/record"
+            className={`${styles.nav_item} ${
+              selectedItem === "/record" ? styles.nav_item_select : ""
+            }`}
+          >
+            지진발생 이력
+          </Link>
+          <Link
+            to="/rule"
+            className={`${styles.nav_item} ${
+              selectedItem === "/rule" ||
+              selectedItem === "/rule/subpage1" ||
+              selectedItem === "/rule/subpage2"
+                ? styles.nav_item_select
+                : ""
+            }`}
+          >
+            행동요령
+          </Link>
+          {/*       <div className={`${styles.nav_login}`}>
         {isLogin ? (
           <Profile userInfo={userInfo} />
         ) : (
           <Login setUserInfo={setUserInfo} setIsLogin={setIsLogin} />
         )}
       </div> */}
-    </nav>
+        </nav>
+      </PC>
+      <Mobile>
+        <nav className={`${styles.nav} ${isSidebarOpen ? styles.open : ""}`}>
+          <Link to="/" className={styles.nav_title}>
+            지진알리미
+          </Link>
+          <div className={styles.nav_toggle} onClick={toggleHeader}>
+            <img
+              src={menu}
+              alt="menu"
+              width="40px"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+            />
+          </div>
+          <Link
+            to="/shelter"
+            style={{ display: isMenuOpen ? "flex" : "none" }}
+            className={`${styles.nav_item}`}
+          >
+            지진대피소 조회
+          </Link>
+          <Link
+            to="/record"
+            style={{ display: isMenuOpen ? "flex" : "none" }}
+            className={`${styles.nav_item}`}
+          >
+            지진발생이력
+          </Link>
+          <Link
+            to="/rule"
+            style={{ display: isMenuOpen ? "flex" : "none" }}
+            className={`${styles.nav_item}`}
+          >
+            행동요령
+          </Link>
+          {/*       <div className={`${styles.nav_login}`}>
+        {isLogin ? (
+          <Profile userInfo={userInfo} />
+        ) : (
+          <Login setUserInfo={setUserInfo} setIsLogin={setIsLogin} />
+        )}
+      </div> */}
+        </nav>
+      </Mobile>
+    </>
   );
 };
 
