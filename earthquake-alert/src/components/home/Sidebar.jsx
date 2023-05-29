ec733bd869082fdd0068f4e3d03c3909df5352e0
Hankyul Min <denirokr22@gmail.com>
2023-05-29 22:58:10 +0900

    Chore: 토큰 저장 localStorage->sessionStorage

--- earthquake-alert/src/components/home/Sidebar.jsx
+++ earthquake-alert/src/components/home/Sidebar.jsx
@@ -43,27 +43,29 @@ const Sidebar = ({
     query: "(max-width:819px)"
   });
 
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
 
   const checkLogin = () => {
-    if (localStorage.getItem("token") === null) return;
-    if (localStorage.getItem("token")) {
+    if (token === null) return;
+    if (token) {
       fetch("http://localhost:8081/api/user", {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
+          Authorization: `Bearer ${token}`
         }
       })
-        .then((res) => res.json())
-        .then(
-          (res) => {
-            console.log(res);
+        .then((res) => {
+          if (res.ok) {
             setIsLogin(true);
-          },
-          (error) => {
-            console.log(error);
+          } else {
+            setIsLogin(false);
+            window.alert("로그인이 만료되었습니다.");
+            window.location.reload();
           }
-        );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   };
 
@@ -354,7 +356,6 @@ const Sidebar = ({
                     style={{
                       top: `${topValue + 70 * index + additionalOffset}px`
                     }}
-
                     onClick={() => {
                       updateMapCenter(
                         parseFloat(bookmark.placeLat),
@@ -397,7 +398,6 @@ const Sidebar = ({
                             50 * idx
                           }px`
                         }}
-
                         onClick={() => {
                           updateMapCenter(
                             parseFloat(item.shelterLat),
