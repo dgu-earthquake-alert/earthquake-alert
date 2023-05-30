8483d2a85318b303712dc3a89c0c365d8ce4c254
Subin <ben7232@gmail.com>
2023-05-30 13:05:02 +0900

    Chore: 쓰지 않는 의존성 삭제

--- earthquake-alert/src/components/Footer.jsx
+++ earthquake-alert/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/footer.module.css";
-import { Button } from "react-bootstrap";
 
 const Footer = ({ isSidebarOpen, handleTestModalOpen }) => {
   return (
